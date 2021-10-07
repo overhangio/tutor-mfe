9679aacbfb4e6bfc9d4c2664a33acb0b0d820ee9
Régis Behmo <regis@behmo.com>
2021-10-07 15:55:14 +0200

    fix: actually make it possible to run in development
    Includes instructions and plenty of fixes to make it actually possible to run
    MFEs in development.

---
+++ tutormfe/templates/mfe/apps/mfe/webpack.dev.config.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+
+const baseDevConfig = require('@edx/frontend-build/config/webpack.dev.config.js');
+
+module.exports = merge(baseDevConfig, {
+  // This configuration needs to be defined here, because CLI
+  // arguments are ignored by the "npm run start" command
+  devServer: {
+    // We will have to make changes to thix config in later releases of webpack dev devServer
+    // https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md
+    // allowedHosts: ['all'],
+    disableHostCheck: true,
+  },
+})
+
+{{ patch("mfe-webpack-dev-config") }}
