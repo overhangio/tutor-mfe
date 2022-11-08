9481b8b8d19924641399905c4fdef6c26bf8357a
vunguyen-dmt <34919039+vunguyen-dmt@users.noreply.github.com>
2022-11-08 19:40:48 +0700

    Update plugin.py

--- tutormfe/plugin.py
+++ tutormfe/plugin.py
@@ -45,7 +45,7 @@ config = {
             },
         },
         "AUTHN_MFE_APP": {
-            "name": "authn",
+            "name": "login",
             "repository": "https://github.com/openedx/frontend-app-authn",
             "port": 1999
         },
