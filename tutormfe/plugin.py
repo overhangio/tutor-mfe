d6b64fe51327b0724bddd62df05ebbfd0331d111
vunguyen-dmt <34919039+vunguyen-dmt@users.noreply.github.com>
2022-11-09 15:54:27 +0700

    Update plugin.py

--- tutormfe/plugin.py
+++ tutormfe/plugin.py
@@ -51,6 +51,9 @@ config = {
             "env": {
                 "production": {
                     "DISABLE_ENTERPRISE_LOGIN": "true",
+                    "LOGO_URL": "https://dmt-statics.s3.ap-southeast-1.amazonaws.com/images/GoAmazing_Logo.png",
+                    "LOGO_WHITE_URL": "https://dmt-statics.s3.ap-southeast-1.amazonaws.com/images/GoAmazing_Logo.png",
+                    "ALLOW_PUBLIC_ACCOUNT_CREATION": false
                 },
             }
         },
