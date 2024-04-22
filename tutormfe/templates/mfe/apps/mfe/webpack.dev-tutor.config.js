1a789bd6b8734acd749a89b4b253c667282214b1
Emad Rad <codewithemad@gmail.com>
2024-04-22 15:09:27 +0330

    chore: typo fixed

--- tutormfe/templates/mfe/apps/mfe/webpack.dev-tutor.config.js
+++ tutormfe/templates/mfe/apps/mfe/webpack.dev-tutor.config.js
@@ -11,7 +11,7 @@ module.exports = merge(baseDevConfig, {
   // This configuration needs to be defined here, because CLI
   // arguments are ignored by the "npm run start" command
   devServer: {
-    // We will have to make changes to thix config in later releases of webpack dev devServer
+    // We will have to make changes to this config in later releases of webpack dev devServer
     // https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md
     allowedHosts: 'all',
     proxy: {
