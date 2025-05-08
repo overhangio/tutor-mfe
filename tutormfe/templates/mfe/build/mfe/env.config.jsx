40bba5c0c88026157296b28b740c277e61744f4a
Syed Muhammad Dawoud Sheraz Ali <40599381+DawoudSheraz@users.noreply.github.com>
2025-05-08 21:25:33 +0500

    Update tutormfe/templates/mfe/build/mfe/env.config.jsx
    Co-authored-by: Adolfo R. Brandes <adolfo@axim.org>

--- tutormfe/templates/mfe/build/mfe/env.config.jsx
+++ tutormfe/templates/mfe/build/mfe/env.config.jsx
@@ -43,7 +43,7 @@ async function setConfig () {
     {%- endfor %}
 
     {{- patch("mfe-env-config-runtime-final") }}
-  } catch (err) { console.error("env.config.jsx failed to apply.", err);}
+  } catch (err) { console.error("env.config.jsx failed to apply: ", err);}
 
   return config;
 }
